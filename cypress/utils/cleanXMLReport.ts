22eed78f0c3b99b59be4f2da27f8cdbb12894f80
elvis <elvis@elvis.com>
2023-05-25 07:36:16 -0500

    updating cleanxml

--- cypress/utils/cleanXMLReport.ts
+++ cypress/utils/cleanXMLReport.ts
@@ -1,24 +1,20 @@
 import { DOMParser, XMLSerializer } from 'xmldom';
-import * as fs from 'fs';
+import fs = require('fs');
 
-export function cleanXMLReport(filePath: string): void {
+export default function cleanXMLReport(filePath: string): void {
   const xmlData = fs.readFileSync(filePath, 'utf-8');
   const parser = new DOMParser();
   const xmlDoc = parser.parseFromString(xmlData, 'text/xml');
-
   const testcaseElements = Array.from(xmlDoc.getElementsByTagName('testcase'));
-  console.log('elvis');
-  for (const testcase of testcaseElements) {
-    const nameAttr = testcase.getAttribute('name');
-    const classnameAttr = testcase.getAttribute('classname');
-
+  testcaseElements.forEach((testcase) => {
+    const nameAttr = (testcase as Element).getAttribute('name');
+    const classnameAttr = (testcase as Element).getAttribute('classname');
     if (nameAttr && classnameAttr) {
       const newText = nameAttr.match(/\[(.*?)\]/)?.[1] || '';
-      testcase.setAttribute('name', newText);
-      testcase.setAttribute('classname', newText);
+      (testcase as Element).setAttribute('name', newText);
+      (testcase as Element).setAttribute('classname', newText);
     }
-  }
-
+  });
   const serializer = new XMLSerializer();
   const modifiedXML = serializer.serializeToString(xmlDoc);
   fs.writeFileSync(filePath, modifiedXML, 'utf-8');
